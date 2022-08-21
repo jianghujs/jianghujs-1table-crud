2c76746feb1f1a3e32c98073007c7dcf32920cdc
Liu Ji <mynameisliuji@foxmail.com>
2022-08-21 21:45:58 +0800

    doAction.html

--- config/config.default.js
+++ config/config.default.js
@@ -21,7 +21,7 @@ module.exports = appInfo => {
     appLogo: `${appId}/public/img/logo.png`,
     appType: 'single',
     appDirectoryLink: '/',
-    indexPage: `/${appId}/page/uiAction`,
+    indexPage: `/${appId}/page/doUiAction`,
     loginPage: `/${appId}/page/login`,
     helpPage: `/${appId}/page/help`,
     uploadDir,
